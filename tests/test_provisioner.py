e4d8ac8040925b242be728380a39874d8211ddcf
Chris Speck <cgspeck@gmail.com>
2015-08-08 16:33:42 +1000

    Pytest WIP

--- tests/test_provisioner.py
+++ tests/test_provisioner.py
@@ -1,47 +1,43 @@
 import contextlib
 from mock import Mock, patch
 from amibaker.provisioner import Provisioner
+import pytest
+
+@pytest.fixture
+def mock_provisioner():
+    ec2 = Mock()
+    provisioner = Provisioner(ec2, quiet=True)
+    patch('amibaker.provisioner.put', return_value=True),  # NOQA
+    patch('amibaker.provisioner.sudo', return_value=True)
+    return provisioner
 
 
-class TestProvisioner():
-    @classmethod
-    def setup_class(cls):
-        cls.ec2 = Mock()
-        cls.provisioner = Provisioner(cls.ec2, quiet=True)
+@pytest.mark.parametrize("times", [
+    (0),
+    (1),
+    (4),
+])
+def test_many_copys(mock_provisioner, times):
+    copy = []
+    source = []
+    target = []
+    mode = []
 
-    def test_copy(self):
-        self.copy(times=1)
+    for i in xrange(0, times):
+        source.append("/path/to/source%d" % i)
+        target.append("/path/to/target%d" % i)
+        mode.append((i+1) * 222)  # 222, 444, 666
 
-    def test_no_copy(self):
-        self.copy(times=0)
+        copy.append({'from': source[i],
+                     'to': target[i],
+                     'mode': mode[i]})
 
-    def test_multiple_copies(self):
-        self.copy(times=4)
+    mock_provisioner._Provisioner__copy(copy)
 
-    def copy(self, times=1):
-        copy = []
-        source = []
-        target = []
-        mode = []
+    assert put.call_count == times
 
-        for i in xrange(0, times):
-            source.append("/path/to/source%d" % i)
-            target.append("/path/to/target%d" % i)
-            mode.append((i+1) * 222)  # 222, 444, 666
+    calls = [put(source[i], target[i], mode=mode[i])
+             for i in reversed(xrange(0, times))]
 
-            copy.append({'from': source[i],
-                         'to': target[i],
-                         'mode': mode[i]})
+    put.has_calls(calls)
 
-        with contextlib.nested(
-                patch('amibaker.provisioner.put', return_value=True),  # NOQA
-                patch('amibaker.provisioner.sudo', return_value=True)
-            ) as (put, sudo):
-            self.provisioner._Provisioner__copy(copy)
-
-            assert put.call_count == times
-
-            calls = [put(source[i], target[i], mode=mode[i])
-                     for i in reversed(xrange(0, times))]
-
-            put.has_calls(calls)
